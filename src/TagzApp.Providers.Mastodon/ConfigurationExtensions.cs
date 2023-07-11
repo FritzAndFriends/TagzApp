97f69ab813beb4f3a82ab90d049c7c09203b4a97
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-07-11 11:47:10 -0400

    Moved up a folder and added initial Mastodon provider code

---
+++ src/TagzApp.Providers.Mastodon/ConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TagzApp.Providers.Mastodon;
+
+public static class ConfigurationExtensions
+{
+
+	public static IServiceCollection RegisterServices(this IServiceCollection services)
+	{
+
+		services.AddTransient<ISocialMediaProvider, MastodonProvider>();
+
+		return services;
+
+	}
+
+}
---
+++ src/TagzApp.Providers.Mastodon/MastodonProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace TagzApp.Providers.Mastodon;
+
+internal class MastodonProvider : ISocialMediaProvider
+{
+	public string Id => "MASTODON";
+	public string DisplayName => "Mastodon";
+
+	public Task<IEnumerable<Content>> GetContentForHashtag(Hashtag tag, DateTimeOffset since)
+	{
+		throw new NotImplementedException();
+	}
+}
+
+
+
