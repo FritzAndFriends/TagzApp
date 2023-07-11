f39c8e6305c9544e40223bc0d40d6898c98c7524
Rodrigo Alarcón <alarcon.rodrigo@gmail.com>
2023-07-11 14:05:21 -0400

    Took the liberty in removing all those Non-Nullable warnings for something that makes since.

--- src/TagzApp.Common/Creator.cs
+++ src/TagzApp.Common/Creator.cs
@@ -6,26 +6,26 @@ public class Creator
 	/// <summary>
 	/// Provider that this creator is associated with
 	/// </summary>
-  public string ProviderId { get; set; }
+  public string ProviderId { get; set; } = string.Empty;
 
 	/// <summary>
 	/// The screenname for the user
 	/// </summary>
-  public string UserName { get; set; }
+  public string UserName { get; set; } = string.Empty;
 
   /// <summary>
   /// Name of the creator
   /// </summary>
-  public string DisplayName { get; set; }
+  public string DisplayName { get; set; } = string.Empty;
 
-	/// <summary>
-	/// Uri to the profile of the creator
-	/// </summary>
-	public Uri ProfileUri { get; set; }
+  /// <summary>
+  /// Uri to the profile of the creator
+  /// </summary>
+  public required Uri ProfileUri { get; set; }
 
 	/// <summary>
 	/// Uri to the profile image of the creator
 	/// </summary>
-	public Uri ProfileImageUri { get; set; }
+	public required Uri ProfileImageUri { get; set; }
 
 }
