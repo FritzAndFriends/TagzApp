579f46660fd75c963b1118b7b260632c7396bebf
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-07-25 21:06:19 -0400

    Started work on Twitter provider

---
+++ src/TagzApp.Providers.Twitter/TwitterConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace TagzApp.Providers.Twitter;
+
+public class TwitterConfiguration
+{
+
+	/// <summary>
+	/// Twitter issued API Key for the service
+	/// </summary>
+  public string ApiKey { get; set; }
+
+	/// <summary>
+	/// Twitter issued API Secret Key for the service
+	/// </summary>
+  public string ApiSecretKey { get; set; }
+
+}
---
+++ src/TagzApp.Providers.Twitter/TwitterProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TagzApp.Common;
+
+namespace TagzApp.Providers.Twitter;
+
+public class TwitterProvider : ISocialMediaProvider
+{
+	private readonly HttpClient _Client;
+
+	public string Id => "TWITTER";
+	public string DisplayName => "Twitter";
+	public TimeSpan NewContentRetrievalFrequency => TimeSpan.FromSeconds(15);
+
+  public TwitterProvider(HttpClient client)
+  {
+		_Client = client;
+  }
+
+  public Task<IEnumerable<Content>> GetContentForHashtag(Hashtag tag, DateTimeOffset since)
+	{
+		throw new NotImplementedException();
+	}
+
+
+}
+
+public class StartTwitter : IConfigureProvider
+{
+
+	public IServiceCollection RegisterServices(IServiceCollection services, IConfiguration configuration)
+	{
+		throw new NotImplementedException();
+	}
+
+}
