4aae9c76250d5fa8ec2107d6909ca4707477da0c
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-07-26 16:44:44 -0400

    Added error checking for configuration of Mastodon and Twitter providers

--- src/TagzApp.Providers.Twitter/StartTwitter.cs
+++ src/TagzApp.Providers.Twitter/StartTwitter.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using TagzApp.Common.Exceptions;
 
 namespace TagzApp.Providers.Twitter;
 
@@ -12,8 +13,23 @@ public class StartTwitter : IConfigureProvider
 	public IServiceCollection RegisterServices(IServiceCollection services, IConfiguration configuration)
 	{
 
-		IConfiguration config = configuration.GetSection(ConfigurationKey);
-		services.Configure<TwitterConfiguration>(config);
+		IConfiguration config = null;
+		try
+		{
+			config = configuration.GetSection(ConfigurationKey);
+			services.Configure<TwitterConfiguration>(config);
+		} catch (Exception ex) {
+
+			// Was not able to configure the provider
+			throw new InvalidConfigurationException(ex.Message, ConfigurationKey);
+
+		}
+
+		if (config is null || string.IsNullOrEmpty(config.GetValue<string>("ApiKey")))
+		{
+			// No configuration provided, no registration to be added
+			return services;
+		}
 
 		services.AddHttpClient<ISocialMediaProvider, TwitterProvider>((svc, c) =>
 		{
