d0903f6937c43f8014e63e1994f71c36c2ecc7b2
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-08-08 11:26:20 -0400

    Introduced a TagzAppNavigator abstraction for the WebTests

---
+++ src/TagzApp.WebTest/TagzAppNavigator.cs
@@ -0,0 +1,38 @@
+﻿namespace TagzApp.WebTest;
+
+/// <summary>
+/// A collection of extension methods that navigate the application.
+/// </summary>
+public static class TagzAppNavigator
+{
+
+	public static async Task<IPage> GotoHashtagSearchPage(this IPage page)
+	{
+
+		await page.GotoAsync("/");
+
+		return page;
+
+	}
+
+	public static async Task<IPage> SearchForHashtag(this IPage page, string hashtag)
+	{
+
+		await page.GetByPlaceholder("New Hashtag").FillAsync(hashtag);
+
+		await page.GetByRole(AriaRole.Button, new() { Name = "Add" }).ClickAsync();
+
+		return page;
+
+	}
+
+	public static async Task<IPage> GotoWaterfallPage(this IPage page)
+	{
+
+		if (page.Url != "/") await page.GotoAsync("/");
+
+		return page;
+
+	}
+
+}
