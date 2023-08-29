711fd35bd7b79865dbf7d22d6f03674735bed12c
Tero Kilpeläinen <degenone1@gmail.com>
2023-08-29 15:08:36 +0300

    reverting all formatting for now

--- src/TagzApp.Web/wwwroot/js/site.js
+++ src/TagzApp.Web/wwwroot/js/site.js
@@ -1,231 +1,196 @@
 ﻿(function () {
-  var connection;
 
-  const taggedContent = document.getElementById("taggedContent");
-  const observer = new MutationObserver(function (mutationsList, observer) {
-    for (const mutation of mutationsList) {
-      if (mutation.type !== "childList") continue;
-      for (const node of mutation.addedNodes) {
-        const imgCardTop = node.querySelector(".card-img-top");
-        if (imgCardTop !== null) {
-          imgCardTop.addEventListener(
-            "load",
-            function (ev) {
-              window.Masonry.resizeGridItem(node);
-            },
-            false,
-          );
-          imgCardTop.addEventListener(
-            "error",
-            function (ev) {
-              window.Masonry.resizeGridItem(node);
-            },
-            false,
-          );
-        } else {
-          window.Masonry.resizeGridItem(node);
-        }
-      }
-    }
-  });
-  const observerConfig = {
-    childList: true,
-  };
+	var connection;
 
-  observer.observe(taggedContent, observerConfig);
-  async function start() {
-    try {
-      await connection.start();
-      console.log("SignalR Connected.");
-    } catch (err) {
-      console.log(err);
-      setTimeout(start, 5000);
-    }
+	const taggedContent = document.getElementById("taggedContent");
+	const observer = new MutationObserver(function (mutationsList, observer) {
+		for (const mutation of mutationsList) {
+			if (mutation.type !== 'childList') continue;
+			for (const node of mutation.addedNodes) {
+				const imgCardTop = node.querySelector(".card-img-top");
+				if (imgCardTop !== null) {
+					imgCardTop.addEventListener("load", function (ev) {
+						window.Masonry.resizeGridItem(node);
+					}, false);
+					imgCardTop.addEventListener("error", function (ev) {
+						window.Masonry.resizeGridItem(node);
+					}, false);
+				} else {
+					window.Masonry.resizeGridItem(node);
+				}
+            }
+		}
+	});
+	const observerConfig = {
+		childList: true
+	};
 
-    connection.onclose(async () => {
-      await start();
-    });
-  }
+	observer.observe(taggedContent, observerConfig);
+	async function start() {
+		try {
+			await connection.start();
+			console.log("SignalR Connected.");
+		} catch (err) {
+			console.log(err);
+			setTimeout(start, 5000);
+		}
 
-  function FormatMessage(content) {
-    if (taggedContent.querySelector(".spinner-border")) {
-      taggedContent.querySelector(".spinner-border").remove();
-    }
+		connection.onclose(async () => {
+			await start();
+		});
 
-    if (
-      document.querySelector("[data-providerid='" + content.providerId + "']")
-    )
-      return;
+	};
 
-    const newMessage = document.createElement("article");
-    newMessage.setAttribute("data-url", content.sourceUri);
-    newMessage.setAttribute("data-provider", content.provider);
-    newMessage.setAttribute("data-providerid", content.providerId);
-    const newMessageTime = new Date(content.timestamp);
-    newMessage.setAttribute("data-timestamp", newMessageTime.toISOString());
-    newMessage.innerHTML = `
-		<img class="ProfilePicture" src="${content.authorProfileImageUri}" alt="${
-      content.authorDisplayName
-    }" />
+	function FormatMessage(content) {
+
+		if (taggedContent.querySelector(".spinner-border")) {
+			taggedContent.querySelector(".spinner-border").remove();
+		}
+
+		if (document.querySelector("[data-providerid='" + content.providerId + "']")) return;
+
+		const newMessage = document.createElement("article");
+		newMessage.setAttribute("data-url", content.sourceUri);
+		newMessage.setAttribute("data-provider", content.provider);
+		newMessage.setAttribute("data-providerid", content.providerId);
+		const newMessageTime = new Date(content.timestamp);
+		newMessage.setAttribute("data-timestamp", newMessageTime.toISOString());
+		newMessage.innerHTML = `
+		<img class="ProfilePicture" src="${content.authorProfileImageUri}" alt="${content.authorDisplayName}" />
 		<div class="byline">
 			<div class="author">${content.authorDisplayName}</div>
-			<div class="authorUserName" title="${content.authorUserName}">${
-        content.authorUserName
-      }</div>
+			<div class="authorUserName" title="${content.authorUserName}">${content.authorUserName}</div>
 		</div>
 		<i class="provider bi bi-${content.provider.toLowerCase()}"></i>
-		<div class="time">${newMessageTime.toLocaleString(undefined, {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-      hour: "numeric",
-      minute: "2-digit",
-    })}</div>
+		<div class="time">${newMessageTime.toLocaleString(undefined, { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: '2-digit' })}</div>
 
 		<div class="content">${content.text}</div>`;
 
-    if (content.previewCard) {
-      newMessage.innerHTML += `
+		if (content.previewCard) {
+			newMessage.innerHTML += `
 				<div class="contentcard">
 					<img src="${content.previewCard.imageUri}" class="card-img-top" alt="${content.previewCard.altText}" />
 				</div>
-			`;
-    }
+			`
+		}
 
-    newMessage.addEventListener("click", function (ev) {
-      var el = ev.target.closest("article");
+		newMessage.addEventListener("click", function (ev) {
 
-      connection.invoke(
-        "SendMessageToOverlay",
-        window.TagzApp.Tags[0],
-        el.getAttribute("data-provider"),
-        el.getAttribute("data-providerid"),
-      );
+			var el = ev.target.closest('article');
 
-      // Format Modal
-      let modalProfilePic = document.querySelector(".modal-header img");
-      modalProfilePic.src = content.authorProfileImageUri;
-      modalProfilePic.alt = content.authorDisplayName;
+			connection.invoke("SendMessageToOverlay", window.TagzApp.Tags[0], el.getAttribute("data-provider"), el.getAttribute("data-providerid"));
 
-      document.querySelector(".modal-header .author").innerText =
-        content.authorDisplayName;
+			// Format Modal
+			let modalProfilePic = document.querySelector(".modal-header img");
+			modalProfilePic.src = content.authorProfileImageUri;
+			modalProfilePic.alt = content.authorDisplayName;
 
-      let modalAuthorUserName = document.querySelector(
-        ".modal-header .authorUserName",
-      );
-      modalAuthorUserName.setAttribute("title", content.authorUserName);
-      modalAuthorUserName.innerText = content.authorUserName;
+			document.querySelector(".modal-header .author").innerText = content.authorDisplayName;
 
-      let modalProvider = document.querySelector(".modal-header .bi");
-      modalProvider.setAttribute("class", "");
-      modalProvider.classList.add(
-        "provider",
-        "bi",
-        `bi-${content.provider.toLowerCase()}`,
-      );
+			let modalAuthorUserName = document.querySelector(".modal-header .authorUserName");
+			modalAuthorUserName.setAttribute("title", content.authorUserName);
+			modalAuthorUserName.innerText = content.authorUserName;
 
-      document.querySelector(
-        ".modal-header .time",
-      ).innerText = `${newMessageTime.toLocaleString(undefined, {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "2-digit",
-      })}`;
+			let modalProvider = document.querySelector(".modal-header .bi");
+			modalProvider.setAttribute("class", "");
+			modalProvider.classList.add('provider', 'bi', `bi-${content.provider.toLowerCase()}`);
 
-      let modalBody = (document.querySelector(".modal-body").innerHTML =
-        content.text);
+			document.querySelector(".modal-header .time").innerText = `${newMessageTime.toLocaleString(undefined, { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: '2-digit' })}`;
 
-      if (content.previewCard) {
-        document.querySelector(".modal-body").innerHTML += `
+			let modalBody = document.querySelector(".modal-body").innerHTML = content.text;
+
+			if (content.previewCard) {
+				document.querySelector(".modal-body").innerHTML += `
 				<div class="contentcard">
 					<img src="${content.previewCard.imageUri}" class="card-img-top" alt="${content.previewCard.altText}" />
 				</div>
-			`;
-      }
+			`
+			}
 
-      let modalWindow = new bootstrap.Modal(
-        document.getElementById("contentModal"),
-      );
-      modalWindow.show();
-    });
+			let modalWindow = new bootstrap.Modal(document.getElementById("contentModal"));
+			modalWindow.show();
 
-    const newest = getDateFromElement(taggedContent.firstElementChild);
-    const oldest = getDateFromElement(taggedContent.lastElementChild);
-    if (newest === null || newest <= newMessageTime) {
-      taggedContent.prepend(newMessage);
-    } else if (oldest !== null && oldest > newMessageTime) {
-      taggedContent.append(newMessage);
-    } else {
-      const times = [...taggedContent.children].map((article) =>
-        getDateFromElement(article),
-      );
-      const index = getIndexForValue(times, newMessageTime);
-      taggedContent.insertBefore(newMessage, taggedContent.children[index]);
+		});
+
+		const newest = getDateFromElement(taggedContent.firstElementChild);
+		const oldest = getDateFromElement(taggedContent.lastElementChild);
+		if (newest === null || (newest <= newMessageTime)) {
+			taggedContent.prepend(newMessage);
+		} else if (oldest !== null && (oldest > newMessageTime)) {
+			taggedContent.append(newMessage);
+		} else {
+			const times = [...taggedContent.children].map(article => getDateFromElement(article));
+			const index = getIndexForValue(times, newMessageTime);
+			taggedContent.insertBefore(newMessage, taggedContent.children[index]);
+		}
+
+	}
+
+	function getDateFromElement(el) {
+		const timestamp = el?.dataset.timestamp;
+		if (timestamp === undefined || timestamp === null) return null;
+
+		const date = new Date(timestamp);
+		if (isNaN(date)) return null;
+
+		return date;
+	}
+
+	function getIndexForValue(list, value) {
+		let low = 0;
+        let high = list.length - 1;
+
+        while (low <= high) {
+			let mid = (low + high) >>> 1;
+			let guess = list[mid];
+
+			if (guess === value) return mid;
+
+			if (guess > value) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+
+        return low;
     }
-  }
 
-  function getDateFromElement(el) {
-    const timestamp = el?.dataset.timestamp;
-    if (timestamp === undefined || timestamp === null) return null;
+	const t = {
 
-    const date = new Date(timestamp);
-    if (isNaN(date)) return null;
+		Tags: [],
 
-    return date;
-  }
+		ListenForTags: async function (tags) {
 
-  function getIndexForValue(list, value) {
-    let low = 0;
-    let high = list.length - 1;
+			var tagCsv = encodeURI(tags);
+			t.Tags = tags.split(",");
 
-    while (low <= high) {
-      let mid = (low + high) >>> 1;
-      let guess = list[mid];
+			connection = new signalR.HubConnectionBuilder()
+				.withUrl(`/messages?t=${tagCsv}`)
+				.withAutomaticReconnect()
+				.configureLogging(signalR.LogLevel.Information)
+				.build();
 
-      if (guess === value) return mid;
+			connection.on("NewMessage", (content) => {
+				FormatMessage(content);
+			});
 
-      if (guess > value) {
-        low = mid + 1;
-      } else {
-        high = mid - 1;
-      }
-    }
+			// Start the connection.
+			await start();
 
-    return low;
-  }
+			connection.invoke("GetExistingContentForTag", tags)
+				.then(function (result) {
 
-  const t = {
-    Tags: [],
+					result.forEach(function (content) {
+						FormatMessage(content);
+					});
+					window.Masonry.resizeAllGridItems();
+				});
 
-    ListenForTags: async function (tags) {
-      var tagCsv = encodeURI(tags);
-      t.Tags = tags.split(",");
+		},
 
-      connection = new signalR.HubConnectionBuilder()
-        .withUrl(`/messages?t=${tagCsv}`)
-        .withAutomaticReconnect()
-        .configureLogging(signalR.LogLevel.Information)
-        .build();
+	};
 
-      connection.on("NewMessage", (content) => {
-        FormatMessage(content);
-      });
+	window.TagzApp = window.TagzApp || t;
 
-      // Start the connection.
-      await start();
-
-      connection
-        .invoke("GetExistingContentForTag", tags)
-        .then(function (result) {
-          result.forEach(function (content) {
-            FormatMessage(content);
-          });
-          window.Masonry.resizeAllGridItems();
-        });
-    },
-  };
-
-  window.TagzApp = window.TagzApp || t;
 })();
