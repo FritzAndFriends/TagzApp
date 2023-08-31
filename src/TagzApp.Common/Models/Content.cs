4610197c27911264e6ff381b94c526f68b08fce7
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2023-08-31 16:39:14 +0200

    created first TODO for testing todo-to-issue GitHub Action

--- src/TagzApp.Common/Models/Content.cs
+++ src/TagzApp.Common/Models/Content.cs
@@ -15,7 +15,10 @@ public class Content
 	/// <summary>
 	///   Id provided by the provider for this content
 	/// </summary>
+	// TODO: CS8618: Non-nullable field is uninitialized. Consider declaring as nullable.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 	public string ProviderId { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 	public string HashtagSought { get; set; } = string.Empty;
 
