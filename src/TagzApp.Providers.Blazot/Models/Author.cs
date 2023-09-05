1f3a0eced90dc1640ebe6ea8c538d86b6469789d
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2023-09-05 20:31:35 +0200

    cleanup: Providers.Blazot.Models.Author CS8618 -- Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.

--- src/TagzApp.Providers.Blazot/Models/Author.cs
+++ src/TagzApp.Providers.Blazot/Models/Author.cs
@@ -1,9 +1,15 @@
 ﻿namespace TagzApp.Providers.Blazot.Models;
+
+// TODO: Check CS8618: Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 public class Author
 {
-  public string UserName { get; set; }
-  public string DisplayName { get; set; }
-  public bool IsSubscriber { get; set; }
-  public int? SubscriptionLevel { get; set; }
-  public string ProfileImageUrl { get; set; }
+
+	public string UserName { get; set; }
+
+	public string DisplayName { get; set; }
+	public bool IsSubscriber { get; set; }
+	public int? SubscriptionLevel { get; set; }
+	public string ProfileImageUrl { get; set; }
 }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
