6bde4df4832eccebd3a6e0f4b37a9de9bc9e9858
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2023-09-05 17:26:56 +0200

    cleanup: Providers.Twitter/Models/Tweet.cs
    (cherry picked from commit d4c77975454ef418cc23d26c6afacfc2d8c54f9e)

--- src/TagzApp.Providers.Twitter/Models/Tweet.cs
+++ src/TagzApp.Providers.Twitter/Models/Tweet.cs
@@ -1,7 +1,10 @@
 ﻿namespace TagzApp.Providers.Twitter.Models;
 
+// TODO: Check all these CS8618: Non-nullable properties
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 public class TwitterData
 {
+
 	public Tweet[] data { get; set; }
 	public Includes includes { get; set; }
 	public Meta meta { get; set; }
@@ -116,3 +119,4 @@ public class Attachments
 {
 	public string[] media_keys { get; set; }
 }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
