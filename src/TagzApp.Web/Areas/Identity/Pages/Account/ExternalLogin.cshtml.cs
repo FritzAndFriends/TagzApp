0074bccbab27ed7a041b362bd6de011ad9849a9a
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-09-16 12:22:52 -0400

    Updated to .NET 8

--- src/TagzApp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/TagzApp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -2,21 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
 
 namespace TagzApp.Web.Areas.Identity.Pages.Account
 {
