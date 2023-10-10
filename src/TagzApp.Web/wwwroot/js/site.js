2ccdf13c9ad3e0bdfa25bb62352ecc3a699e4ba3
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-10-10 12:27:27 -0400

    Added handling for content accepted / rejected while in pause status

--- src/TagzApp.Web/wwwroot/js/site.js
+++ src/TagzApp.Web/wwwroot/js/site.js
@@ -527,13 +527,37 @@
 			});
 
 			connection.on('NewApprovedMessage', (content) => {
-				ApproveMessage(content);
-				// Approve message if its in the pause queue
+
+				if (!paused) {
+					ApproveMessage(content);
+				} else {
+
+					// Find item in the pauseQueue and set its state to ModerationState.Approved
+					pauseQueue.forEach(function (item) {
+						if (item.providerId == content.providerId) {
+							item.state = ModerationState.Approved;
+						}
+					});
+
+				}
+
 			});
 
 			connection.on('NewRejectedMessage', (content) => {
-				RejectMessage(content);
-				// Reject message if its in the pause queue
+
+				if (!paused) {
+					RejectMessage(content);
+				} else {
+
+					// find item in the pauseQueue and set its state to ModerationState.Rejected
+					pauseQueue.forEach(function (item) {
+						if (item.providerId == content.providerId) {
+							item.state = ModerationState.Rejected;
+						}
+					});
+
+				}
+
 			});
 
 			connection.on('NewModerator', (moderator) => {
