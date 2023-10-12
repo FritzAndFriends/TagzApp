d045b6126a3b8169d2950b4242c0a83296a965a6
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-10-12 16:29:29 -0400

    Allocated portraitOverlay (#239)

---
+++ src/TagzApp.Web/Pages/PortraitOverlay.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TagzApp.Web.Pages
+{
+    public class PortraitOverlayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
