8137b1432458690c4fb261b6700b217ca2ee9416
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-03-09 19:07:45 -0500

    Removed Common requirement, started URL

--- src/TagzApp.TwitchRelay/Function1.cs
+++ src/TagzApp.TwitchRelay/Function1.cs
@@ -1,16 +1,19 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace TagzApp.TwitchRelay;
 
 public class Function1
 {
+	private readonly IConfiguration _Configuration;
 	private readonly ILogger _logger;
 
-	public Function1(ILoggerFactory loggerFactory)
+	public Function1(IConfiguration configuration, ILoggerFactory loggerFactory)
 	{
+		_Configuration = configuration;
 		_logger = loggerFactory.CreateLogger<Function1>();
 	}
 
@@ -26,4 +29,22 @@ public class Function1
 
 		return response;
 	}
+
+	[Function("GetLoginUrl")]
+	public HttpResponseData GetLoginUrl(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetLoginUrl")] HttpRequestData req
+	)
+	{
+		var response = req.CreateResponse(HttpStatusCode.OK);
+		response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+		response.WriteString($"https://id.twitch.tv/oauth2/authorize?" +
+			$"client_id={_Configuration["TwitchClientId"]}&" +
+			$"redirect_uri={_Configuration["TwitchRedirectUri"]}&" +
+			"response_type=code&scope=channel:bot+user:bot+chat:read+user:read:chat");
+
+		return response;
+
+	}
+
 }
