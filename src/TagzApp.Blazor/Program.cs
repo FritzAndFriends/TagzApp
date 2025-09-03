2b0ca7d0a19719a6652dd8908c360282dd2028ff
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-09-02 21:13:48 -0400

    Error pages, about page, privacy policy (#496)

--- src/TagzApp.Blazor/Program.cs
+++ src/TagzApp.Blazor/Program.cs
@@ -116,6 +116,7 @@ public class Program
 		else
 		{
 			app.UseExceptionHandler("/Error", createScopeForErrors: true);
+			app.UseStatusCodePagesWithReExecute("/Error/{0}");
 			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 			app.UseHsts();
 			app.UseResponseCompression();
